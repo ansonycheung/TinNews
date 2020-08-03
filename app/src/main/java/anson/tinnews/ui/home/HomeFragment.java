46da729cf347549af6e61aca42598da297bb9594
anson <zhang.yang6@husky.neu.edu>
2020-08-02 18:02:34 -0700

    Completes home page implementation.

--- app/src/main/java/anson/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/anson/tinnews/ui/home/HomeFragment.java
@@ -12,7 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mindorks.placeholderview.SwipeDecor;
+
 import anson.tinnews.R;
+import anson.tinnews.databinding.FragmentHomeBinding;
+import anson.tinnews.model.Article;
 import anson.tinnews.repository.NewsRepository;
 import anson.tinnews.repository.NewsViewModelFactory;
 
@@ -22,6 +26,7 @@ import anson.tinnews.repository.NewsViewModelFactory;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel viewModel;
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -33,13 +38,29 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     // forms a new screen that covers old screen
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //  use the getBuilder() method to modify the default swipeView configurations.
+        //  In this example we are adding 3 cards in the display.
+        //  SwipeDecor class is used to adjust the visual elements of the view.
+        //  Here paddingTop and relativeScale gives the perception of a card being placed in stack
+        binding.swipeView
+                .getBuilder()
+                .setDisplayViewCount(3)
+                .setSwipeDecor(new SwipeDecor().setPaddingTop(20)
+                .setRelativeScale(0.01f));
+
+        binding.rejectBtn.setOnClickListener(v -> binding.swipeView.doSwipe(false));
+        binding.acceptBtn.setOnClickListener(v -> binding.swipeView.doSwipe(true));
+
+
 
         NewsRepository repository = new NewsRepository(getContext());
         viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
@@ -52,6 +73,11 @@ public class HomeFragment extends Fragment {
                         newsResponse -> {
                             if (newsResponse != null) {
                                 Log.d("HomeFragment", newsResponse.toString());
+                                // Retrofit Response and add to the PlaceHolderView
+                                for (Article article : newsResponse.articles) {
+                                    TinNewsCard tinNewsCard = new TinNewsCard(article);
+                                    binding.swipeView.addView(tinNewsCard);
+                                }
                             }
                         });
 
---
+++ app/src/main/java/anson/tinnews/ui/home/TinNewsCard.java
@@ -0,0 +1,71 @@
+package anson.tinnews.ui.home;
+
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mindorks.placeholderview.annotations.Layout;
+import com.mindorks.placeholderview.annotations.Resolve;
+import com.mindorks.placeholderview.annotations.View;
+import com.mindorks.placeholderview.annotations.swipe.SwipeCancelState;
+import com.mindorks.placeholderview.annotations.swipe.SwipeIn;
+import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
+import com.squareup.picasso.Picasso;
+
+import anson.tinnews.R;
+import anson.tinnews.model.Article;
+
+// @layout is used to bind the layout with this class.
+@Layout(R.layout.tin_news_card)
+public class TinNewsCard {
+
+    // @View is used to bind the views in a layout we want to refer to.
+    @View(R.id.news_image)
+    private ImageView image;
+
+    // @View is used to bind the views in a layout we want to refer to.
+    @View(R.id.news_title)
+    private TextView newsTitle;
+
+    // @View is used to bind the views in a layout we want to refer to.
+    @View(R.id.news_description)
+    private TextView newsDescription;
+
+    private final Article article;
+
+    public TinNewsCard(Article news) {
+        this.article = news;
+    }
+
+    // @Resolve annotation binds a method to be executed when the view is ready to be used.
+    // Any operation we want to perform on view references should be written
+    // in a method and annotated with this.
+    @Resolve
+    private void onResolved() {
+        newsTitle.setText(article.title);
+        newsDescription.setText(article.description);
+        if (article.urlToImage == null || article.urlToImage.isEmpty()) {
+            image.setImageResource(R.drawable.ic_empty_image);
+        } else {
+            Picasso.get().load(article.urlToImage).into(image);
+        }
+    }
+
+    // @SwipeOut calls the annotated method when the card has been rejected.
+    @SwipeOut
+    private void onSwipedOut() {
+        Log.d("EVENT", "onSwipedOut");
+    }
+
+    @SwipeCancelState
+    private void onSwipeCancelState() {
+        Log.d("EVENT", "onSwipeCancelState");
+    }
+
+    // @SwipeIn calls the annotated method when the card has been accepted.
+    @SwipeIn
+    private void onSwipeIn() {
+        Log.d("EVENT", "onSwipedIn");
+    }
+}
+
